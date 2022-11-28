ebe3513171e09096ecd636be76c9fb3e7d0cfcb7
ellananto <94938554+ellananto@users.noreply.github.com>
2022-11-28 14:26:10 -0700

    enter

--- intex/kidneyfoundation/urls.py
+++ intex/kidneyfoundation/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import indexPageView
+from . import views
 from .views import aboutPageView
 from .views import showUserPageView
 from .views import updateUserPageView
@@ -10,5 +10,5 @@ urlpatterns = [
     path("about/", aboutPageView, name="about"),
     path("updateUser/", updateUserPageView, name='updateUser'),
     path("addUser/", addUserPageView, name='addUser'),
-    path("", indexPageView, name="index"),
+    path('', views.indexPageView, name='dashboard-index'),
 ]
