9583c04dca53f1419b25c431702fcafb5cd88630
ellananto <94938554+ellananto@users.noreply.github.com>
2022-11-28 22:59:37 -0700

    enter

--- intex/intex/settings.py
+++ intex/intex/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kidneyfoundation',
         'USER': 'postgres',
-        'PASSWORD': 'ENTER HERE',
+        'PASSWORD': 'glimmer133',
         'HOST': 'localhost'
     }
 }
@@ -120,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'intex/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
