93ae867714a82e2f758b5d687549185ced5114df
ellananto <94938554+ellananto@users.noreply.github.com>
2022-11-30 13:37:54 -0700

    enter

--- intex/intex/settings.py
+++ intex/intex/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kidneyfoundation.apps.KidneyfoundationConfig',
-    'users.apps.UsersConfig',
     'crispy_forms',
 
 ]
