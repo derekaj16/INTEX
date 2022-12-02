38435795d9b0207af5e1dd4c3390aafc52e44f15
derekaj16 <71414948+derekaj16@users.noreply.github.com>
2022-12-01 19:47:08 -0700

    Delete intex directory

--- intex/kidneyfoundation/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Food)
-admin.site.register(Comorbidity)
-admin.site.register(Entry)
